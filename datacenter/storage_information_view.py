4d142a4c30de5c1ee23a79b5b376dccf10a34b1a
olberd <olberd@bk.ru>
2022-09-24 17:36:56 +0300

    A bad commit needs to be split into several commits

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,21 @@
-from datacenter.models import Passcard
+from datacenter.models import Passcard, format_duration
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits = []
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    visits = Visit.objects.filter(leaved_at=None)
+    for visit in visits:
+        non_closed_visit = {
+            'who_entered': visit.passcard,
+            'entered_at': visit.entered_at,
+            'duration': format_duration(Visit.get_duration(visit)),
+            'is_strange': visit.is_visit_long(minutes=60)
         }
-    ]
+        non_closed_visits.append(non_closed_visit)
+
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
